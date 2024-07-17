6e46325dcba4b2e7114209b166e1934913f2dbf7
Mohammad Shukuhi <mohammadhassanshukuhimd@gmail.com>
2024-07-18 00:42:23 +0330

    commit3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 function App() {
-  return <h1>React Tic-Tac-Toe</h1>;
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player">
+            Player 1
+            <span className="player-symbol">X</span>
+            <button>Edit</button>
+          </span>
+        </li>
+      </ol>
+    </div>
+  </main>;
 }
 
 export default App;
