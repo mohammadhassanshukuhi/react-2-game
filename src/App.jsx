907ffa6166368e8e3e5d4fa9f6a7eb1ef37ff9e0
Mohammad Shukuhi <mohammadhassanshukuhimd@gmail.com>
2024-07-20 11:20:07 +0330

    board2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import GameBoard from "./component/GameBoard";
 import Player from "./component/Player";
 
-
 function App() {
-  return <main>
-    <div id="game-container">
-      <ol id="players">
-        <Player initialName={"Player 1"} symbol={"X"} />
-        <Player initialName={"Player 2"} symbol={"O"} />
-      </ol>
-      <GameBoard />
-    </div>
-  </main>;
+  const [player, setPlayer] = useState("X");
+
+  function handelSetTurn() {
+    setPlayer((prevTurn) => (prevTurn === "X" ? "O" : "X"));
+  }
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName={"Player 1"}
+            symbol={"X"}
+            isActive={player === "X"}
+          />
+          <Player
+            initialName={"Player 2"}
+            symbol={"O"}
+            isActive={player === "O"}
+          />
+        </ol>
+        <GameBoard turn={player} onSlectSquare={handelSetTurn} />
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/component/GameBoard.jsx
+++ src/component/GameBoard.jsx
@@ -1,27 +1,40 @@
-const initialGameBoard = 
-[
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
+import { useState } from "react";
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
+export default function GameBoard({ onSlectSquare, turn }) {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
+  function handlSquareClick(rowIndex, colIndex) {
+    setGameBoard((currentGameBoard) => {
+      const updateBoard = [
+        ...currentGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updateBoard[rowIndex][colIndex] = turn;
+      onSlectSquare();
+      return updateBoard;
+    });
+  }
 
-export default function GameBoard() {
-    return(
-        <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (
-                <li>
-                    <ol>
-                        {row.map((playerSymbol, colIndex) => (
-                            <li>
-                                <button>{playerSymbol}</button>
-                            </li>
-                        ))}
-                    </ol>
-                </li>
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rI) => (
+        <li key={rI}>
+          <ol>
+            {row.map((playerSymbol, cI) => (
+              <li key={cI}>
+                <button onClick={() => handlSquareClick(rI, cI)}>
+                  {playerSymbol}
+                </button>
+              </li>
             ))}
-        </ol>
-    )
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
 }
